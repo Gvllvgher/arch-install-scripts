e5145113a0dd3f0f9aa406d927343065406b524a
Justin Gallagher <justingallagher@outlook.com>
2022-06-15 10:04:26 -0400

    directory issues with git clone

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ cp inside-chroot.sh /mnt/temp/ > /dev/null
 echo "Copied inside-chroot.sh to /mnt"
 
 # Clone arch-customization-scripts to /temp
-git clone https://github.com/Gvllvgher/arch-customization-scripts /mnt/temp > /dev/null
+git clone https://github.com/Gvllvgher/arch-customization-scripts /mnt/temp/ > /dev/null
 
 # Change all .sh file permissoins
 chmod +x /mnt/temp/**/*.sh > /dev/null
