88c95d0984d3f81935888e0bac51334d255eba4c
Justin Gallagher <justingallagher@outlook.com>
2022-06-14 23:00:03 -0400

    Removed minimal installs, it's all minimal now

--- inside-chroot.sh
+++ inside-chroot.sh
@@ -1,21 +1,27 @@
 #! /bin/bash
 
 # Install Grub
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub > /dev/null
+echo "Installed Grub"
+grub-mkconfig -o /boot/grub/grub.cfg > /dev/null
+echo "Generated Grub configuration"
 
 # Set the timezone
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime > /dev/null
+echo "Set Timezone to America/New_York"
 
 # Sync system clocks
-hwclock --systohc
+hwclock --systohc > /dev/null
+echo "System clock sync"
 
 # Generate Locale
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen > /dev/null
+locale-gen > /dev/null
+echo "Generated locale"
 
 # Set locale in /etc/locale.conf
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf > /dev/null
 
 # Set hostname in /etc/hostname
-echo "arch" >> /etc/hostname
+echo "arch" >> /etc/hostname > /dev/null
+echo "Set Hostname"
