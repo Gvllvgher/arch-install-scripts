8fede1955c3bae5f74cdb06a85e86f83f06f7524
Justin Gallagher <justingallagher@outlook.com>
2022-06-14 20:52:06 -0400

    Install scripts incoming

---
+++ inside-chroot.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Install Grub
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+
+# Set the timezone
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+# Sync system clocks
+hwclock --systohc
+
+# Generate Locale
+locale-gen
+
+# Set locale in /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Set hostname in /etc/hostname
+echo "arch" >> /etc/hostname
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+
+# Setup hardware clock
 timedatectl set-ntp true
-# Can install git on archiso using pacman -Sy git --noconfirm
-#disk stuff
+
+# Disk Partitioning
 sgdisk -Z /dev/sda
 sgdisk -n 0:0:+500M -t 0:ef00 -c 0:"efi" /dev/sda
 sgdisk -n 0:0:+3G -t 0:8200 -c 0:"swap" /dev/sda
@@ -9,23 +11,29 @@ sgdisk -n 0:0:0 -t 0:8300 -c 0:"linux" /dev/sda
 sgdisk -p /dev/sda
 partprobe /dev/sda
 
+# Disk Formatting
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 
+# Mount the disks
 mount /dev/sda3 /mnt
 mount --mkdir /dev/sda1 /mnt/boot
 swapon /dev/sda2
 
-#install
+# Install essential packages
 pacstrap /mnt base linux linux-firmware git wget curl vim nano grub efibootmgr net-tools wpa_supplicant
+
+# Generate fstab with current mount points
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-locale-gen
-touch /etc/locale.conf
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-touch /etc/hostname
-echo "arch" >> /etc/hostname
+
+# Copy inside-chroot.sh to /mnt/
+cp inside-chroot.sh /mnt/
+chmod +X /mnt/inside-chroot.sh
+chmod +777 /mnt/inside-chroot.sh
+
+# Execute inside-chroot.sh
+arch-chroot /mnt /inside-chroot.sh
+
+# This is a test for now
+rm /mnt/inside-chroot.sh
