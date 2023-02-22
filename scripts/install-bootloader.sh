b07e20cd5ab62fb73a0139ed5b079b59a773e752
Justin Gallagher <justingallagher@outlook.com>
2023-02-22 11:30:14 -0500

    systemd-boot script

--- scripts/install-bootloader.sh
+++ scripts/install-bootloader.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
 
-OS_PART="/dev/sda3"
+while getopts ':p:' opt; do
+    case $opt in
+        p)
+            OS_PART=${OPTARG}
+            echo "OS Partition set to: ${OPTARG}"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
 
-#bootctl install
+if [[ -z "$OS_PART" ]]; then
+    echo "Use parameter -p to define an OS partition"
+    exit 1
+fi
+
+bootctl install
 
 cat <<EOT >> /boot/loader/loader.conf
 default arch
@@ -14,5 +34,5 @@ title Arch Linux
 linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
-options root=PARTUUID=$(blkid $OS_PART -s PARTUUID -o value)
+options root=PARTUUID=$(blkid $OS_PART -s PARTUUID -o value) rw
 EOT
