596bbeba70ab8d99a00a013c2e220ce18d2efc53
Justin Gallagher <justingallagher@outlook.com>
2023-03-15 16:50:09 -0400

    discord and sdrpp installers

---
+++ apps/install-discord.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while getopts ':u:' opt; do
+    case $opt in
+        u)
+            LOCAL_USER=${OPTARG}
+            ;;
+        \?)
+            echo "Invalid Option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$LOCAL_USER" ]]; then
+    if ! [[ -z "$SUDO_USER" ]]; then
+        LOCAL_USER=$SUDO_USER
+    else
+        echo "Use -u to specify a user"
+        exit 1
+    fi
+fi
+
+su $LOCAL_USER<<EOF
+set -e
+yay -S discord_arch_electron --noconfirm > /dev/null
+exit
+EOF
